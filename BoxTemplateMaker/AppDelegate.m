142d00c05cfd30794d8caf96369785acab807e65
Maxym <maxym@Airbender.local>
2013-10-29 19:06:02 -0700

    Initial Commit

---
+++ BoxTemplateMaker/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  BoxTemplateMaker
+//
+//  Created by Maxym on 10/29/13.
+//  Copyright (c) 2013 Maxym. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ BoxTemplateMaker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BoxTemplateMaker
+//
+//  Created by Maxym on 10/29/13.
+//  Copyright (c) 2013 Maxym. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
